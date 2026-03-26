5ab8ff0838181a1d34cbd923e6276a83b919c1f9
Arun Sambyal <arunsambyal123216av@gmail.com>
2026-03-26 23:46:56 +0530

    updgrade Dto of  salesorderModule

--- src/main/java/com/example/inventory_management_system/SalesOrderModule/DTO/SalesOrderResponse.java
+++ src/main/java/com/example/inventory_management_system/SalesOrderModule/DTO/SalesOrderResponse.java
@@ -1,5 +1,12 @@
 package com.example.inventory_management_system.SalesOrderModule.DTO;
 
+import lombok.AllArgsConstructor;
+import lombok.Data;
+import lombok.NoArgsConstructor;
+
+@Data
+@AllArgsConstructor
+@NoArgsConstructor
 public class SalesOrderResponse {
 
 }
--- src/main/java/com/example/inventory_management_system/SalesOrderModule/Entity/SalesOrderItem.java
+++ src/main/java/com/example/inventory_management_system/SalesOrderModule/Entity/SalesOrderItem.java
@@ -1,5 +1,16 @@
 package com.example.inventory_management_system.SalesOrderModule.Entity;
 
+import lombok.AllArgsConstructor;
+import lombok.Data;
+import lombok.NoArgsConstructor;
+
+@Data
+@AllArgsConstructor
+@NoArgsConstructor
 public class SalesOrderItem {
+
+    private String productId;
+    private int quantity;
+    private double price;
 
 }
