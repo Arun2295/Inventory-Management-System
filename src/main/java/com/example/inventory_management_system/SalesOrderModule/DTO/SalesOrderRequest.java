30fa9f612e25b6416dbb680672cff4f51840169a
Arun Sambyal <arunsambyal123216av@gmail.com>
2026-03-28 13:09:05 +0530

    worked on salesOrderModule DTO

--- src/main/java/com/example/inventory_management_system/SalesOrderModule/DTO/SalesOrderRequest.java
+++ src/main/java/com/example/inventory_management_system/SalesOrderModule/DTO/SalesOrderRequest.java
@@ -3,11 +3,19 @@ package com.example.inventory_management_system.SalesOrderModule.DTO;
 import lombok.AllArgsConstructor;
 import lombok.Data;
 import lombok.NoArgsConstructor;
+import java.util.List;
 
 @Data
 @AllArgsConstructor
 @NoArgsConstructor
 public class SalesOrderRequest {
     
+    private String customerId;
+    private  List<ItemRequest> items;
+
+    public static class ItemRequest{
+        private String productid;
+        private int quantity;
+    }
 
 }
--- src/main/java/com/example/inventory_management_system/SalesOrderModule/DTO/SalesOrderResponse.java
+++ src/main/java/com/example/inventory_management_system/SalesOrderModule/DTO/SalesOrderResponse.java
@@ -1,5 +1,10 @@
 package com.example.inventory_management_system.SalesOrderModule.DTO;
 
+import java.time.LocalDate;
+
+import com.example.inventory_management_system.SalesOrderModule.Enum.OrderStatus;
+import java.util.List;
+
 import lombok.AllArgsConstructor;
 import lombok.Data;
 import lombok.NoArgsConstructor;
@@ -8,5 +13,20 @@ import lombok.NoArgsConstructor;
 @AllArgsConstructor
 @NoArgsConstructor
 public class SalesOrderResponse {
+
+    private String id;
+    private String customerId;
+    private LocalDate orderDate;
+    private OrderStatus status;
+    private double totalAmount;
+    private List<ItemResponse> items;
+
+    private static class ItemResponse{
+        private String productid;
+        private String productname;
+        private double price;
+        private int quantity;
+
+    }
 
 }
