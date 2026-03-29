7fc07ce84bc6a5fbf51c1e67bbb22eade3deaab3
Arun Sambyal <arunsambyal123216av@gmail.com>
2026-03-29 17:00:11 +0530

    completed DTOs class of PurchaseOrder Module

--- src/main/java/com/example/inventory_management_system/PurchaseOrderModule/DTO/PurchaseOrderRequest.java
+++ src/main/java/com/example/inventory_management_system/PurchaseOrderModule/DTO/PurchaseOrderRequest.java
@@ -1,5 +1,28 @@
 package com.example.inventory_management_system.PurchaseOrderModule.DTO;
 
+import lombok.AllArgsConstructor;
+import lombok.Data;
+import lombok.NoArgsConstructor;
+
+import java.time.LocalDate;
+import java.util.List;
+
+@Data
+@AllArgsConstructor
+@NoArgsConstructor
 public class PurchaseOrderRequest {
+
+    private String supplierId;
+    private List<ItemRequest> items;
+    private LocalDate expectedDeliveryDate;
+
+    @Data
+    @AllArgsConstructor
+    @NoArgsConstructor
+    public static class ItemRequest{
+        private String productId;
+        private int quantity;
+    }
+
 
 }
--- src/main/java/com/example/inventory_management_system/PurchaseOrderModule/DTO/PurchaseOrderResponse.java
+++ src/main/java/com/example/inventory_management_system/PurchaseOrderModule/DTO/PurchaseOrderResponse.java
@@ -1,5 +1,30 @@
 package com.example.inventory_management_system.PurchaseOrderModule.DTO;
 
+import java.time.LocalDate;
+
+import com.example.inventory_management_system.PurchaseOrderModule.Enum.PurchaseOrderStatus;
+
+import lombok.AllArgsConstructor;
+import lombok.Data;
+import lombok.NoArgsConstructor;
+import java.util.List;
+@Data
+@AllArgsConstructor
+@NoArgsConstructor
 public class PurchaseOrderResponse {
+
+    private String id;
+    private String supplierId;
+    private List<ItemResponse> items;
+    private LocalDate expectedDeliveryDate;
+    private PurchaseOrderStatus status;
+
+    @Data
+    @AllArgsConstructor
+    @NoArgsConstructor
+    public static class ItemResponse{
+        private String productId;
+        private int quantity;
+    }
 
 }
