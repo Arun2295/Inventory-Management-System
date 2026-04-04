add87a3d34a51a780037f336d1d731a801c1038e
Arun Sambyal <arunsambyal123216av@gmail.com>
2026-04-04 19:31:43 +0530

    Implemted Invoice Module REST APIs

---
+++ src/main/java/com/example/inventory_management_system/InvoiceGeneration/Controller/InvoiceController.java
@@ -0,0 +1,42 @@
+package com.example.inventory_management_system.InvoiceGeneration.Controller;
+
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.http.MediaType;
+import org.springframework.http.ResponseEntity;
+import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.PathVariable;
+import org.springframework.web.bind.annotation.PostMapping;
+import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.RestController;
+
+import com.example.inventory_management_system.InvoiceGeneration.DTO.InvoiceResponse;
+import com.example.inventory_management_system.InvoiceGeneration.Service.InvoiceServiceInterface;
+import java.util.List;
+
+@RestController
+@RequestMapping("/invoices")
+public class InvoiceController {
+
+    @Autowired
+    private  InvoiceServiceInterface invoiceService;
+
+    @GetMapping
+    public ResponseEntity<List<InvoiceResponse>> getAllInvoices(){
+        List<InvoiceResponse> response = invoiceService.getAllInvoices();
+        return ResponseEntity.ok(response);
+    }
+
+    @PostMapping("/{salesOrderId}")
+    public ResponseEntity<InvoiceResponse> generateInvoice(@PathVariable String salesOrderId){
+        InvoiceResponse response  = invoiceService.generateInvoice(salesOrderId);
+        return ResponseEntity.ok(response);
+    }
+
+    @GetMapping("/{id}/pdf")
+    public ResponseEntity<byte[]> generateInvoicePDF(@PathVariable String id){
+        byte[] pdf = invoiceService.generateInvoicePDF(id);
+        return ResponseEntity.ok().header("Content Disposition", "attachment; filename=invoice.pdf").contentType(MediaType.APPLICATION_PDF).body(pdf);
+    }
+
+
+}
