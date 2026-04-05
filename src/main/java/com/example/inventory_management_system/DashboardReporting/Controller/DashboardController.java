c2054d768244b84ec5ec0ae6834704ede23468ef
Arun Sambyal <arunsambyal123216av@gmail.com>
2026-04-05 15:09:49 +0530

    Implemented REST APIs of REporting Module

---
+++ src/main/java/com/example/inventory_management_system/DashboardReporting/Controller/DashboardController.java
@@ -0,0 +1,34 @@
+package com.example.inventory_management_system.DashboardReporting.Controller;
+
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.http.ResponseEntity;
+import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.RestController;
+
+import com.example.inventory_management_system.DashboardReporting.Service.ReportingServiceImpl;
+import com.example.inventory_management_system.DashboardReporting.Service.ReportingServiceInterface;
+
+@RestController
+@RequestMapping("/dashboard")
+public class DashboardController {
+
+    @Autowired
+    private  ReportingServiceInterface reportingInterface;
+
+    @GetMapping
+    public ResponseEntity<?> getSalesSummary(){
+        return ResponseEntity.ok(reportingInterface.getSalesSummary());
+    }
+
+    @GetMapping
+    public ResponseEntity<?> getPurchaseSummary(){
+        return ResponseEntity.ok(reportingInterface.getPurchaseSummary());
+    }
+
+    @GetMapping
+    public ResponseEntity<?> getStockAlert(){
+        return ResponseEntity.ok(reportingInterface.getStockAlert());
+    }
+
+}
