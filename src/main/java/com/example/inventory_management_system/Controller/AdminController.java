0e12d25a61aad3a016e376ef1347c4b23016c230
Arun Sambyal <arunsambyal123216av@gmail.com>
2026-04-07 01:27:55 +0530

    assign Role based access to dashboards

--- src/main/java/com/example/inventory_management_system/Controller/AdminController.java
+++ src/main/java/com/example/inventory_management_system/Controller/AdminController.java
@@ -2,6 +2,7 @@ package com.example.inventory_management_system.Controller;
 
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.http.ResponseEntity;
+import org.springframework.security.access.prepost.PreAuthorize;
 import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.PathVariable;
 import org.springframework.web.bind.annotation.PutMapping;
@@ -22,17 +23,20 @@ public class AdminController {
     private UserService userService;
 
     @GetMapping()
+    @PreAuthorize("hasRole('ADMIN')")
     public ResponseEntity<?> getAllRoles(@PathVariable Role role){
         return ResponseEntity.ok().body(userService.getUserByRole(role));
     }
 
     @GetMapping("/all-users")
+    @PreAuthorize("hasRole('ADMIN')")
     public ResponseEntity<?> getAllUsers(){
         return ResponseEntity.ok().body(userService.getAllUsers());
     }
 
 
     @PutMapping("/assign-role")
+    @PreAuthorize("hasRole('ADMIN')")
     public ResponseEntity<?> assignRole(@RequestBody UpdateRoleRequest roleRequest){
         return ResponseEntity.ok().body(userService.assignRole(roleRequest));
     }
